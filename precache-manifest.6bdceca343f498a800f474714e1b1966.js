26322b7aea6b3e3da07ab08754846512d194656b
Vanessa Leung <leungkachiiiii@gmail.com>
2020-04-06 21:59:26 -0700

    Updates

---
+++ precache-manifest.6bdceca343f498a800f474714e1b1966.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28a7e799e0e143188184dcb9090d527c",
+    "url": "/corona-map-react/index.html"
+  },
+  {
+    "revision": "426f18399e659e5d4ee7",
+    "url": "/corona-map-react/static/css/main.25069232.chunk.css"
+  },
+  {
+    "revision": "3a9e2796a2fbdc09881e",
+    "url": "/corona-map-react/static/js/2.48022bc8.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/corona-map-react/static/js/2.48022bc8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "426f18399e659e5d4ee7",
+    "url": "/corona-map-react/static/js/main.fa73e169.chunk.js"
+  },
+  {
+    "revision": "4d8e6c39a22acb531038",
+    "url": "/corona-map-react/static/js/runtime-main.97f074bb.js"
+  }
+]);
