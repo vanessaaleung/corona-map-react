ff9d2cc1629b55ffd7d18bc19dc27f40f21fbfa6
Eric <leungkachiiiii@gmail.com>
2020-07-07 18:56:44 -0700

    Updates

--- precache-manifest.366a1411c73ad636539361be85daa33a.js
+++ precache-manifest.366a1411c73ad636539361be85daa33a.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "204262deb4b83fcbec3323c6dbbe03fd",
+    "revision": "c390252baa61b930ddf38cdd7129d92c",
     "url": "/corona-map-react/index.html"
   },
   {
-    "revision": "8fa492823c818abda95d",
+    "revision": "1c42fde39523a6e6b2cf",
     "url": "/corona-map-react/static/css/main.25069232.chunk.css"
   },
   {
-    "revision": "a12eced0abd1315ce4a5",
-    "url": "/corona-map-react/static/js/2.997339c4.chunk.js"
+    "revision": "dca905df7ddf3cff4c39",
+    "url": "/corona-map-react/static/js/2.ea9ae104.chunk.js"
   },
   {
     "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
-    "url": "/corona-map-react/static/js/2.997339c4.chunk.js.LICENSE.txt"
+    "url": "/corona-map-react/static/js/2.ea9ae104.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "8fa492823c818abda95d",
-    "url": "/corona-map-react/static/js/main.03fbd630.chunk.js"
+    "revision": "1c42fde39523a6e6b2cf",
+    "url": "/corona-map-react/static/js/main.17579bce.chunk.js"
   },
   {
     "revision": "4d8e6c39a22acb531038",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/corona-map-react/precache-manifest.365f43aef18f04d72611f651f4ef40ea.js"
+  "/corona-map-react/precache-manifest.366a1411c73ad636539361be85daa33a.js"
 );
 
 self.addEventListener('message', (event) => {
